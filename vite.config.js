9f3ba22c212c200e654146ba426a7f07104e45cc
MickeyNptr <138649941+MickeyNptr@users.noreply.github.com>
2025-03-28 01:14:16 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,17 @@ export default defineConfig({
       host: 'localhost',
       port: 64999,
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        theme: 'theme/assets/theme.js'
+      },
+      output: {
+        entryFileNames: assetInfo => {
+          return `assets/${assetInfo.name}.js`
+        },
+      }
+    }
   }
 });
